c66e3a498a2fce56a6c9c2d71a57e7e899feaf3e
Antonio Pintado <antoniopl99612@gmail.com>
2018-03-12 19:00:43 +0100

    Clase tiovivo

---
+++ src/com/company/Tiovivo.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Tiovivo extends Atraccion {
+
+    int asientos = 20;
+    Atraccion tiovivo = new Atraccion();
+
+    void tiovivo(){
+        tiovivo.availability = true;
+        tiovivo.maxHeight = 2.5;
+        tiovivo.minHeight = 0.5;
+        tiovivo.price = 2;
+    }
+
+
+}
