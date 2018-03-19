2e271a136d959695fedaed2e06aa56492eee454c
Doble-De <ddelgados96@elpuig.xeill.net>
2018-03-19 18:30:25 +0100

    He modificado cosas en la classe cliente, he hecho lo de la classe donación y acabo de crear la classe entrada.

--- src/com/company/Entrada.java
+++ src/com/company/Entrada.java
@@ -4,6 +4,18 @@ public class Entrada {
     Cliente cliente = new Cliente();
 
     void imprimirentrada(){
+        System.out.println("*********************************************************************");
+        System.out.println("*                                                                   *");
+        System.out.println("*                                                                   *");
+        System.out.println("*                                                                   *");
+        System.out.println("*                                                                   *");
+        System.out.println("*                                                                   *");
+        System.out.println("*                                                                   *");
+        System.out.println("*                                                ▏ ▏ ▏▏ ▏▏  ▏▏▏ ▏ ▏▏ ▏ ▏▏ ▏▏ ▏▏▏▏ ▏  *");
+        System.out.println("*                                                ▏ ▏ ▏▏ ▏▏  ▏▏▏ ▏ ▏▏ ▏ ▏▏ ▏▏ ▏▏▏ ▏  *");
+        System.out.println("*                                                ▏ ▏ ▏▏ ▏▏  ▏▏▏ ▏ ▏▏ ▏ ▏▏ ▏▏ ▏▏▏ ▏  *");
+        System.out.println("*                                                                   *");
+        System.out.println("*********************************************************************");
 
     }
 }
