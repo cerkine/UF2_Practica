9b1d0e8ce533cccbc515efd1442360ad69b60411
Yago <yagomoralesares@gmail.com>
2018-03-20 22:53:08 +0100

    Arreglo Clienteview/GestionCliente Comentario para arrelgar null

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -25,7 +25,11 @@ public class Main {
 
         ClienteView clienteView = new ClienteView();
         do{
-            cliente=clienteView.decision();// para ver que cliente es y poder jugar con el innecesario por ahora
+            cliente=clienteView.decision();/*
+            Esto puede llegar a dar un null, podemos hacer que el cliente[99] lo cargemos, ponemos una variable boolean en cliente que sea null=false, si esto nos va a devolver un null, podemos iniciarliar
+            ese cliente y lo cargamos con null true, aqui podmos hacer que si if(cliente.null) haga una cosa y sino pos que haga otra, asi nos evitamos que el main pete si no hay espacio de usuario o si no introducen
+            bien la contraseña*/
+
             System.out.println(cliente.name);
         }while (true);
 
