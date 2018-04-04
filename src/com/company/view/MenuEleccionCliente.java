f758b2afde62125a223cb84db3e757a7191b44ba
Law <tlaw4444@gmail.com>
2018-04-04 18:51:21 +0200

    Merge David y creado imprimir entrada en el menu

--- src/com/company/view/MenuEleccionCliente.java
+++ src/com/company/view/MenuEleccionCliente.java
@@ -8,11 +8,14 @@ import java.util.Scanner;
 public class MenuEleccionCliente {
     public int opcion;
     Scanner scanner = new Scanner(System.in);
+    ViewAlturaAtraccionCliente viewAlturaAtraccionCliente= new ViewAlturaAtraccionCliente();
+    Entrada entrada = new Entrada();
     public void menuCliente(Cliente cliente, Atraccion[] lista){
         System.out.println("Que quieres hacer ?");
         System.out.println("1) Mirar las atracciones que estan abiertas");
         System.out.println("2) Mirar a las atracciones que puede entrar");
         System.out.println("3) Consultar una atraccion especifica");
+        System.out.println("4) Imprimir entrada");
         opcion = scanner.nextInt();
         scanner.nextLine();
 
@@ -21,10 +24,13 @@ public class MenuEleccionCliente {
 
               break;
           case 2:
-              ViewAlturaAtraccionCliente viewAlturaAtraccionCliente= new ViewAlturaAtraccionCliente();
+
               viewAlturaAtraccionCliente.CompararAltura(cliente,lista);
               break;
           case 3:
+              break;
+          case 4:
+              entrada.imprimirentrada(cliente);
               break;
       }
     }
